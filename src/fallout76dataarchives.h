e52c7dc1a9004ca6aebc37b130a4063646b07414
Mikaël Capelle <capelle.mikael@gmail.com>
2020-08-01 19:53:34 +0200

    Fix DataArchives game feature.

--- src/fallout76dataarchives.h
+++ src/fallout76dataarchives.h
@@ -18,12 +18,12 @@ public:
 public:
 
   virtual QStringList vanillaArchives() const override;
-  virtual QStringList Fallout76DataArchives::sResourceIndexFileList() const;
-  virtual QStringList Fallout76DataArchives::sResourceStartUpArchiveList() const;
-  virtual QStringList Fallout76DataArchives::SResourceArchiveMemoryCacheList() const;
-  virtual QStringList Fallout76DataArchives::SResourceArchiveList() const;
-  virtual QStringList Fallout76DataArchives::SResourceArchiveList2() const;
-  virtual QStringList Fallout76DataArchives::sResourceArchive2List() const;
+  virtual QStringList sResourceIndexFileList() const;
+  virtual QStringList sResourceStartUpArchiveList() const;
+  virtual QStringList SResourceArchiveMemoryCacheList() const;
+  virtual QStringList SResourceArchiveList() const;
+  virtual QStringList SResourceArchiveList2() const;
+  virtual QStringList sResourceArchive2List() const;
   virtual QStringList archives(const MOBase::IProfile *profile) const override;
 
 private:
